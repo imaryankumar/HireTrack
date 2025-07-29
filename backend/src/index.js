22330959942bc4f1d0cdb78e4a94b91fa008225d
imaryankumar <aryan8910kumar@gmail.comclear>
2025-07-29 19:34:50 +0530

    Fix:ENV Change

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,7 +33,7 @@ app.use(helmet());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
